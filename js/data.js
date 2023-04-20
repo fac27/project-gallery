058321cfbab7916d8c4e3349d6c9f659e7990d27
Psydwinder <101563800+Psydwinder@users.noreply.github.com>
2023-04-20 13:16:54 +0100

    Initial commit

---
+++ js/data.js
@@ -0,0 +1,86 @@
+console.log("data.js");
+
+const data = {
+  weeks: [
+// Example week object
+//     {
+//       week: 0,
+//       theme: "Tech for Better",
+//       projects: [
+//         {
+//           title: "My Body Back",
+//           img:
+//             "https://camo.githubusercontent.com/aaba1067da6b5933715e09d85a8de74dc36d6a29/68747470733a2f2f692e696d6775722e636f6d2f41474e4e6c74532e706e67",
+//           team: ["Sarah", "Francesca", "Gregor", "Martha"],
+//           github: "https://github.com/fac-17/My-Body-Back",
+//           description:
+//             "The app aims to provide support for people with experience of sexual assault. The app will encourage more people to go for a routince cervical screening (smear test)."
+//         },
+//         {
+//           title: "Autsera",
+//           img:
+//             "https://camo.githubusercontent.com/2a9d11d1232074bd3cee98ef6f3681a6deba0ef0/68747470733a2f2f692e696d6775722e636f6d2f4f5335617179472e706e67",
+//           team: ["Jan", "Reuben", "Leonie", "Victor"],
+//           github: "https://github.com/fac-17/Autsera",
+//           description:
+//             "An interactive game that makes social communication training for children with Autism effective, accessible, affordable and fun."
+//         }
+//       ],
+//     },
+    {
+      week: 1,
+      theme: "Server",
+      projects: [],
+    },
+    {
+      week: 2,
+      theme: "Database",
+      projects: [],
+    },
+    {
+      week: 3,
+      theme: "Authentication",
+      projects: [],
+    },
+    {
+      week: 4,
+      theme: "Server-Side App",
+      projects: [],
+    },
+    {
+      week: 5,
+      theme: "Client-Side App",
+      projects: [],
+    },
+    {
+      week: 6,
+      theme: "Full-stack App",
+      projects: [],
+    },
+    {
+      week: 7,
+      theme: "In-House Final Projects (Weeks 7-9)",
+      projects: [],
+    },
+    {
+      week: 10,
+      theme: "Tech for Better Projects (Weeks 10-12)",
+      projects: [],
+    },
+  ],
+  
+//   Example people array
+//   people: [
+//     {
+//       name: "Reuben",
+//       handle: "reubengt",
+//       img: "https://avatars0.githubusercontent.com/u/42008435"
+//     },
+//     {
+//       name: "Gregor",
+//       handle: "Albadylic",
+//       img: "https://avatars1.githubusercontent.com/u/43413852"
+//     }
+//   ]
+  people: [],
+};
